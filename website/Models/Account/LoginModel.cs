36ccb3c2c95209c04d3f33bf5a19c5d8fd8f572c
swap769 <swap769@gmail.com>
2023-01-12 19:56:06 +0530

    Add project files.

---
+++ website/Models/Account/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace website.Models.Account
+{
+    public class LoginModel
+    {
+        [Required, EmailAddress]
+        public String? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+    }
+}
---
+++ website/Models/Account/Users.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace website.Models.Account
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Name")]
+        public string? Name { get; set; }
+
+        //[Required(ErrorMessage = "Please Enter Your Email"), EmailAddress]
+        [Required(ErrorMessage = "Please enter email"), DataType(DataType.EmailAddress),
+            RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Invalid Email")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "please enter password"),
+            RegularExpression(@"(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$",
+            ErrorMessage = "Minimum 8 character with uparcase,lowercase alphabets,numbers and special symbols")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Address")]
+        public string? Address { get; set; }
+    }
+}
